3323f36a1c03b26ae0905069fbe5eb2dca75c36c
Diego Sanchez <diego.sanches@gmail.com>
2014-08-24 11:12:48 -0300

    ejercicio próxima clase

---
+++ 201402c/clase00/ejercicio_00.c
@@ -0,0 +1,21 @@
+/************
+ * Ejercicio próxima clase 
+ *  Instalar entorno y experimentar con printf (%d, %f, \n \", etc.)
+ *  Imprimir las siguientes salidas:
+ *    - Como hago para imprimir este caracter "?
+ *    - Y este otro %?
+ *    - Imprimir 1, 0002, 0004, 1.6180
+ *
+ *  SUGERENCIAS: 
+ *    investigar sobre función printf
+ *
+ */
+
+int main(int argc, char* argv[] ) {
+  // Aquí código
+  // ...
+  // ...
+  
+  return 0;
+}
+
