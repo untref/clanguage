5e339ef8d44dc9ad28fe2d92e02f42c9ea48626a
diego <diego.sanches@gmail.com>
2014-08-28 13:37:06 -0300

    More code examples

--- 201402c/clase01/main_floating_point.c
+++ 201402c/clase01/main_floating_point.c
@@ -4,10 +4,19 @@ int main(int argc, char** argv) {
   float f = 25.96875;   // 0x41cfc000
   unsigned char* p = &f;
 
+  printf( "Floating point: %f\n", f );
+
   // Checkout the output...
   for( int i = 0; i < sizeof(f); ++i ) {
     printf( "%x\n", p[i] );
   }
 
+  p[0] = 0x0;
+  p[1] = 0x0;
+  p[2] = 0x0;
+  p[3] = 0xff;
+
+  printf( "Floating point: %f\n", f );
+  
   return 0;
 }
