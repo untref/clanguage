5919a27ac17281e3fb61d53537a89782ce106055
diego <diego.sanches@gmail.com>
2014-08-28 13:44:05 -0300

    Some fixes

--- 201402c/clase01/main_floating_point.c
+++ 201402c/clase01/main_floating_point.c
@@ -11,12 +11,5 @@ int main(int argc, char** argv) {
     printf( "%x\n", p[i] );
   }
 
-  p[0] = 0x0;
-  p[1] = 0x0;
-  p[2] = 0x0;
-  p[3] = 0xff;
-
-  printf( "Floating point: %f\n", f );
-  
   return 0;
 }
