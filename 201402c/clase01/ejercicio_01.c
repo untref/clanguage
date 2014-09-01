e4fad41b4bb20de1e4fcb44b7ec70d96d76811b2
diego <diego.sanches@gmail.com>
2014-09-01 10:30:50 -0300

    Exercise 01 for next class

---
+++ 201402c/clase01/ejercicio_01.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/************
+ * Ejercicio próxima clase 
+ *
+ */
+
+int main(int argc, char* argv[] ) {
+  // EJERCICIO 0
+  // Dados los strings s0 y s1, copiar el contenido de uno en el otro
+  //
+  // SUGERENCIA:
+  //  Crear arrays lo suficientemente grandes
+
+  //
+  // Codigo aquí
+  //
+  
+  // Verificación
+  // printf( "s0: %s\n", s0 );
+  // printf( "s1: %s\n", s0 );
+  
+  // EJERCICIO 1
+  //
+  // Dada frase, contar la cantidad de palabras que esta tiene
+  //
+  // SUGERENCIA:
+  //  ' ' es el ascii para el espacio 
+
+  //
+  // Codigo aquí
+  //
+  
+  // Verificación
+  // printf( "frase: %s\n", frase);
+  // printf( "cantidad palabras: %d\n", cantidadPalabras);
+  
+  // EJERCICIO 2
+  // 
+  // Dada los array de flotantes floats00 y floats01 (ambas de la misma longitud)
+  // copiar su contenido de una a otra
+  //
+  
+  // EJERCICIO FINAL
+  // 
+  // Dado que el código del ejercicio 0 y 2 son prácticamente el mismo. 
+  // Escribir un único código que funcione para ambos arrays.
+  //
+  // SUGERENCIAS
+  //  Utilizar void*
+  //
+  
+  
+  return 0;
+}
+
