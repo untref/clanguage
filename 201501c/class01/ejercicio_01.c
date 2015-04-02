a391e0de6226a7e264ac78324fc7dc4be29600f3
diego sanchez <diegos@taohome.(none)>
2015-04-02 20:28:58 -0300

    Pointer exercises

--- 201501c/class01/ejercicio_01.c
+++ 201501c/class01/ejercicio_01.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 
-int main(int argc, char** ) {
+#define TAM_ARRAY 4
+ 
+int main(int argc, char** argv) {
   int enteros[] = { 1, 2, 3, 78 };
-
   int alumnos = 7;
-
   int* pi = 0;
 
-  printf("que significa este valor y cuanto vale: %d\n", pi );
+  printf("que significa este valor y cuanto vale: 0x%x\n", pi );
 
   pi = &alumnos;
 
-  printf("que significa este valor y cuanto vale: %d\n", pi );
+  printf("que significa este valor y cuanto vale: 0x%x\n", pi );
+
+  pi = enteros;
+
+  printf("cual es el valor de la siguiente expresión %d?\n", *pi);
+
+  int* pj = enteros;
+
+  // Que es lo que está haciendo este bloque de código?
+  {
+    int valor = *(pj+2);
+
+    pi = pi + 3;
+
+    pj[2] = *pi;
+
+    *pi = valor;
+  }
+
+  // Imprimir array enteros (sin utilizar los corchetes ni la expresión FOR)!!!!
+
+
 }
