fc40c67b86b44304792d6c33cce3a68870ec63c1
Diego Sanchez <dsanchez@iamat.com>
2015-10-06 15:04:52 -0300

    [class05] sumatoria & productoria exercise

--- 201502c/class05/ejercicio_integrador_1/main.c
+++ 201502c/class05/ejercicio_integrador_1/main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../../assertion.h"
+
+double sumatoria(void* numeros, unsigned total_bytes, unsigned tam_elem,  void (*sumar)(void*, void*) ) {
+    void* current = numeros;
+    double result = 0;
+
+    while ( (current - numeros) < total_bytes ) {
+        sumar(&result, current);
+        current += tam_elem;
+    }
+
+    return result;
+};
+
+void productoria( void* numeros,
+                  unsigned total_bytes,
+                  unsigned tam_elem,
+                  void* result,
+                  void (*prod)(void*, void*) ) {
+    void* current = numeros;
+
+    while ( (current - numeros) < total_bytes ) {
+        prod(result, current);
+        current += tam_elem;
+    }
+};
+
+void sumar_int(void* result, void* elm) {
+    double* r = (double*)result;
+    int* i = (int*)elm;
+
+    *r += *i;
+};
+
+void sumar_double(void* result, void* elm) {
+    double* r = (double*)result;
+    double* i = (double*)elm;
+
+    *r += *i;
+};
+
+void prod_double(void* result, void* elm) {
+    double* r = (double*)result;
+    double* i = (double*)elm;
+
+    *r *= *i;
+};
+
+void prod_short(void* result, void* elm) {
+    short* r = (short*)result;
+    short* i = (short*)elm;
+
+    *r *= *i;
+};
+
 int main()
 {
 
@@ -14,28 +69,28 @@ int main()
     // UN PROCEDIMIENTO.
     {
         int numeros[] = {1, 2, 3, 4, 5};
-        assert( sumatoria(...), 15);
+        assert( sumatoria(numeros, sizeof(numeros), sizeof(int), sumar_int), 15);
     }
 
     {
-        double numeros[] = {1.0, 2.0, 3.0, 4.0, 5.0};
-        assert( sumatoria(...), 15.0);
+        double numeros[] = {1.0, 2.0, 3.0, 4.0, 9.0};
+        assert( sumatoria(numeros, sizeof(numeros), sizeof(double), sumar_double), 19.0);
     }
 
     {
         double numeros[] = { 1.0, 2.0, 3.0};
-        double resultado;
+        double resultado = 1;
 
-        productoria(numeros, sizeof(numeros), sizeof(double), ...);
+        productoria(numeros, sizeof(numeros), sizeof(double), &resultado, prod_double);
 
         assert(resultado, 6.0);
     }
 
     {
         short numeros[] = { 3, 4, 5};
-        short resultado;
+        short resultado = 1;
 
-        productoria(numeros, sizeof(numeros), sizeof(double), ...);
+        productoria(numeros, sizeof(numeros), sizeof(short), &resultado, prod_short);
 
         assert(resultado, 60);
     }
