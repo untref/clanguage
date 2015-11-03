5beb6ec32833976fda932c5916ef66371f54d5f1
Diego Sanchez <dsanchez@iamat.com>
2015-11-02 23:00:26 -0300

    [class08] Another exercise

---
+++ 201502c/class08/MapaBits/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+/****
+* Implementar una clase que permita operar con n bits
+*
+* Se pide:
+* 1.- Poder negar un bit en particular
+* 2.- Poder encender un bit en particular
+* 2.- Poder apagar un bit en particular
+* 3.- Hacer un shift a derecha e izquierda
+* 4.- Poder invertir un bit
+* 5.- Preguntar si sus bits cumplen con una mascar de n bits.
+* 6.- Q se pueda duplicar el array de bits
+*
+* Restricciones:
+*  - No se puede utilizar el operador []
+*
+* En todos los casos debe manejarse la memoria de manera apropiada según los critérios que
+* vimos durante la cursada.
+*/
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
