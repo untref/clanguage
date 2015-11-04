7aaa3e7856c7b26d5a7a0fcc0de2a847a9133062
Diego Sanchez <dsanchez@iamat.com>
2015-11-04 10:01:49 -0300

    [class08] Even more exercises

---
+++ 201502c/class08/repaso_01/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/***
+* Se necesita crear una estructura cuyos miembros son:
+*  - un char
+*  - un dobule
+*  - un short
+*
+* se busca optimizar el espacio que ocupa en memoria (minimizarlo), como definiría
+* la estructura para q ocupe la menor cantidad de bytes posibles en la memoria (en memoria)?
+*
+* Considerar las siguientes alineaciones:
+* - double 8
+* - char 1
+* - short 2
+*/
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ 201502c/class08/repaso_02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char unEnteroDe8Bits = 0xf2;
+    unsigned char otroEnteroDe8Bits = 0xf2;
+
+    // Cual será la salida de este printf?
+    printf("unEnteroDe8Bits: %d\n", unEnteroDe8Bits);
+
+    // Cual será la salida de este printf?
+    printf("otroEnteroDe8Bits: %d\n", otroEnteroDe8Bits);
+    return 0;
+}
